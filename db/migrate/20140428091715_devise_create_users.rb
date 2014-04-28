54bb8c79045aa073a194253f3292ec963201831b
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-04-28 13:00:44 +0200

    Deviation default set to 350

--- db/migrate/20140428091715_devise_create_users.rb
+++ db/migrate/20140428091715_devise_create_users.rb
@@ -23,7 +23,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Rating
       t.integer :number_of_games, null: false, default: 0
       t.decimal :rating,          null: false, default: 1500
-      t.decimal :deviation,       null: false, default: 0
+      t.decimal :deviation,       null: false, default: 350
       t.decimal :volatality,      null: false, default: 0
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140428091715) do
     t.string   "last_sign_in_ip"
     t.integer  "number_of_games",        default: 0,      null: false
     t.decimal  "rating",                 default: 1500.0, null: false
-    t.decimal  "deviation",              default: 0.0,    null: false
+    t.decimal  "deviation",              default: 350.0,  null: false
     t.decimal  "volatality",             default: 0.0,    null: false
     t.string   "country",                default: "",     null: false
     t.datetime "created_at"
