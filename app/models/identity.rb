305bf5567a51a4b72910b8c1785e691c476ed55d
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-03 19:10:44 +0200

    Submit match layout

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,15 @@
 class Identity < ActiveRecord::Base
   belongs_to :faction
+
+  def self.corporations
+    Identity.where(
+      faction_id: Faction.where(is_corporation: true).pluck(:id)
+    )
+  end
+
+  def self.runners
+    Identity.where(
+      faction_id: Faction.where(is_runner: true).pluck(:id)
+    )
+  end
 end
