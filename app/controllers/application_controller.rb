7dea753924d0db93b75d06049f26a059e46bc220
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-06 18:59:52 +0200

    Permitting fullname param on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def index
   end
 
@@ -14,5 +16,9 @@ class ApplicationController < ActionController::Base
   protected
     def not_found
       raise ActionController::RoutingError.new('Not found')
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :fullname
     end
 end
