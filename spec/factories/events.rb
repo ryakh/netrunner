2d1c7439a4f48d3fcf903b55f5ca772b25e26cac
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-06 20:57:14 +0200

    Factories tweaked

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :event do
-    started_at  '2014-05-01 12:21:43'
-    finished_at '2014-05-08 12:21:43'
+    started_at  DateTime.now.beginning_of_week
+    finished_at DateTime.now.end_of_week
   end
 end
--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -1,19 +1,18 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :match do
-    played_on "2014-05-02"
-    first_player nil
-    second_player nil
-    first_player_corporation nil
-    first_player_runner nil
-    second_player_corporation nil
-    second_player_runner nil
-    first_player_corporation_points 1
-    first_player_runner_points 1
-    second_player_corporation_points 1
-    second_player_runner_points 1
-    first_player_league_points 1
-    second_player_league_points 1
+    played_on                '2014-05-02'
+
+    association :first_player,  factory: :user
+    association :second_player, factory: :user
+
+    association :first_player_corporation,  factory: :identity
+    association :first_player_runner,       factory: :identity
+    association :second_player_corporation, factory: :identity
+    association :second_player_runner,      factory: :identity
+
+    first_player_corporation_points  10
+    first_player_runner_points       10
+    second_player_corporation_points 4
+    second_player_runner_points      4
   end
 end
