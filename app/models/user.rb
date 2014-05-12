12144c1e892c836fcccb047a0742f396a6f884d3
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-12 15:14:32 +0200

    Judges method to return all judges

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.judges
+    return User.where(is_judge: true).pluck(:id)
+  end
 end
