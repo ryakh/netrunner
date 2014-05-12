b1b75ab078a62d79e23f89da70033ce9713145fd
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-12 14:16:26 +0200

    Factories updated to make spec pass

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :event do
-    started_at  DateTime.now.beginning_of_week
-    finished_at DateTime.now.end_of_week
+    started_at  Time.now.beginning_of_week
+    finished_at Time.now.end_of_week
   end
 end
