f1f225a641c80d89b78d59b30abf4b63ef2bc96a
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-20 16:46:05 +0200

    Showing specs on main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
+    @standings = Season.last.current_standings
   end
 
   private
