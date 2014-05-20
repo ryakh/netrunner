7653b1edd70db9829ebc96ab224898f9973a77d3
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-20 16:48:22 +0200

    Fixed failing spec due to nil entities

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe ApplicationController do
   describe 'GET index' do
     it 'returns success' do
+      create(:season)
+      create(:event)
       get :index
       expect(response).to be_success
     end
