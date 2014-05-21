6441208a03d7f7aa54a0605b1131f68772e2bfb1
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-21 15:01:07 +0200

    Capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,31 @@
+# Ensure that bundle is used for rake tasks
+# SSHKit.config.command_map[:rake] = "bundle exec rake"
+
+# set :default_environment, {
+#   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+# }
+
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# Set rbenv specific settings
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+set :application, 'netrunner'
+set :repo_url, 'git@github.com:ryakh/netrunner.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, 'master'
+
+set :deploy_via, :copy
+set :stages, ['production']
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/netrunner'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,7 +40,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/application.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -32,7 +49,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
@@ -40,7 +57,8 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :mkdir, release_path.join("#{current_path}/tmp")
+      execute :touch, release_path.join("#{current_path}/tmp/restart.txt")
     end
   end
 
