2ed2b59c7e8e60913a33c7526db5131c2eaa03e5
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-05-31 11:44:53 +0200

    Removed restarting for now

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,8 +57,8 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      run "if ! [[ -d #{current_path}/tmp]]; mkdir #{current_path}/tmp; fi"
-      execute :touch, release_path.join("#{current_path}/tmp/restart.txt")
+      # run "if ! [[ -d #{current_path}/tmp]]; mkdir #{current_path}/tmp; fi"
+      # execute :touch, release_path.join("#{current_path}/tmp/restart.txt")
     end
   end
 
